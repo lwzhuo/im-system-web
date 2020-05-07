8e4721be38b793c80ac1cc13de32ea7d72fd6240
zhuo <lwzhuo@live.com>
2020-05-07 12:25:35 +0800

    修改错误提示

--- src/utils/exception.js
+++ src/utils/exception.js
@@ -2,7 +2,7 @@ export function outputError(source, error) {
   console.log(error.response ? error.response : error.message)
   source.$message({
     showClose: true,
-    message: '出错了，请按F12查看浏览器日志。',
+    message: '服务异常',
     type: 'error'
   })      
 }
