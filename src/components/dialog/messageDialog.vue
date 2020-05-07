8ff14f59a84f50579918995659c6d4fecd777bb0
zhuo <lwzhuo@live.com>
2020-05-08 05:28:55 +0800

    处理Cannot read property 'disabled' of null 错误

--- src/components/dialog/messageDialog.vue
+++ src/components/dialog/messageDialog.vue
@@ -14,12 +14,12 @@
                 <!-- <svg id="dropdown-icon-selected" t="1528208466548" viewBox="0 0 1024 1024" version="1.1"><path d="M517.688889 796.444444c-45.511111 0-85.333333-17.066667-119.466667-51.2L73.955556 381.155556c-22.755556-22.755556-17.066667-56.888889 5.688888-79.644445 22.755556-22.755556 56.888889-17.066667 79.644445 5.688889l329.955555 364.088889c5.688889 5.688889 17.066667 11.377778 28.444445 11.377778s22.755556-5.688889 34.133333-17.066667l312.888889-364.088889c22.755556-22.755556 56.888889-28.444444 79.644445-5.688889 22.755556 22.755556 28.444444 56.888889 5.688888 79.644445L637.155556 739.555556c-28.444444 39.822222-68.266667 56.888889-119.466667 56.888888 5.688889 0 0 0 0 0z" p-id="1094" fill="#418FD6"></path></svg> -->
               </span>
               <!-- 聊天管理 频道名称修改 频道删除 成员添加和删除 -->
-              <el-dropdown-menu v-if="userChannel.channelType === 2" slot="dropdown">
+              <el-dropdown-menu slot="dropdown">
                 <template>
                   <template v-if="isAdmin">
                     <!-- <el-dropdown-item command="manageMember">成员管理</el-dropdown-item> -->
-                    <!-- <el-dropdown-item command="editTitle" divided>编辑频道标题</el-dropdown-item>
-                    <el-dropdown-item command="editName">重命名频道</el-dropdown-item>
+                    <el-dropdown-item command="editTitle" divided>编辑频道标题</el-dropdown-item>
+                    <!-- <el-dropdown-item command="editName">重命名频道</el-dropdown-item>
                     <el-dropdown-item command="remove" v-if="myId === userChannel.creatorId">删除频道</el-dropdown-item> -->
                   </template>
                   <!-- <el-dropdown-item command="addMember">添加成员</el-dropdown-item> -->
